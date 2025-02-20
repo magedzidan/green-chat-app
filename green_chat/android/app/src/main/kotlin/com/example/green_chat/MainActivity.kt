5984b8c63c341ff2d8e3389ac162ef2fa636c2ed
magedzidan <magedzidan@yahoo.com>
2025-02-20 23:32:07 +0200

    making the body of the project
    making the project body while adding essentials we will need for the project like firebase , images and fonts assests

---
+++ green_chat/android/app/src/main/kotlin/com/example/green_chat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.green_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ green_chat/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:green_chat/core/services/database_service.dart';
+import 'package:green_chat/core/utils/route_utils.dart';
+import 'package:green_chat/firebase_options.dart';
+import 'package:green_chat/ui/screens/SplashScreen.dart';
+import 'package:green_chat/ui/screens/other/user_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // 👈 Add this line before anything else
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      //forr responsitve dimenstion resiszing u need to make it varibale
+
+      builder: (context, child) => ChangeNotifierProvider(
+        create: (context)=>UserProvider(DatabaseSerice()),
+        child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            onGenerateRoute: RouteUtils.onGenerateRoute,
+            home: SplashScreen()),
+      ),
+    );
+  }
+}
