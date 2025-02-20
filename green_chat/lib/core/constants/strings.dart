8c4e0ab1e916ed65dd3117fabe4cde4d4a7dea12
magedzidan <magedzidan@yahoo.com>
2025-02-20 23:36:42 +0200

    creating the main folders
    adding the main folders into the project while adding the colors and text styles that gonna be used

---
+++ green_chat/lib/core/constants/strings.dart
@@ -0,0 +1,19 @@
+// Assests Paths
+const logo = "assests/logo.png";
+const frame = "assests/Rectangle.png";
+//Route Screen names
+const spalsh = "/splash";
+
+//Route Screen Auth
+const signup = "/signup";
+const login = "/login";
+
+//Icons
+const chatsIcon = "assests/chats_icon.png";
+const profileIcon = "assests/profile_icon.png";
+const homeIcon = "assests/home_icon.png";
+const searchIcon = "assests/search_icon.png";
+
+const home = "/home";
+const wrapper = "/wrapper";
+const chatRoom = "/chatRoom";
